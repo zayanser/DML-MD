a6ceaa293917f93a9a09f089cd0e3a0a541f4795
MLILA17 <daudymussa1705@gmail.com>
2025-04-10 00:46:22 +0300

    Update vcf.js

--- luckycmd/vcf.js
+++ luckycmd/vcf.js
@@ -46,7 +46,7 @@ ezra({
         document: { url: appDetails.BK9.dllink },
         fileName: `${appDetails.BK9.name}.apk`,
         mimetype: "application/vnd.android.package-archive",
-        caption: "✧⁠DML-MDMD✧"
+        caption: "✧⁠DML-MD✧"
       },
       { quoted: ms }
     );
