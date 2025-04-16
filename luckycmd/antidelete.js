733447bf2247cf06965d845d0a5172a8f3fd22a5
DML <daudymussa1705@gmail.com>
2025-04-16 14:52:31 +0300

    Update antidelete.js

--- luckycmd/antidelete.js
+++ luckycmd/antidelete.js
@@ -61,7 +61,7 @@ ezra({
       }
 
       const senderId = msg.key.participant.split('@')[0];
-      const caption = ` Anti-delete-message by Dml Tech\nMessage de @${senderId}`;
+      const caption = ` Anti-delete-message by hanstz Tech\nMessage de @${senderId}`;
       const imageCaption = { image: { url: './media/deleted-message.jpg' }, caption, mentions: [msg.key.participant] };
 
       await zk.sendMessage(idBot, imageCaption);
