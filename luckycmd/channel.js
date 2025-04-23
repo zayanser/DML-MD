f973b7787619af9eec609977b1a0afcc47a655ee
DML <daudymussa1705@gmail.com>
2025-04-24 00:36:53 +0300

    Update channel.js

--- luckycmd/channel.js
+++ luckycmd/channel.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { ezra } = require("../fredi/ezra");
 
-ezra({ nomCom: "channel", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+ezra({ nomCom: "channel", reaction: "🚀", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Salut je m\'appelle *DML_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *daudy@musa*';
+    let d = ' developed by *daudy@musa*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029Vb2hoPpDZ4Lb3mSkVI3C';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
