003332c053237c9781773dd2af2f738be283ee83
DML <daudymussa1705@gmail.com>
2025-04-28 23:22:41 +0300

    Update utils.js

--- fredi/utils.js
+++ fredi/utils.js
@@ -11,7 +11,7 @@ var lod = [
 "《 ███████▒▒▒▒▒》50%",
 "《 ██████████▒▒》80%",
 "《 ████████████》100%",
-"Lucky load Completed✅"
+"Dml load Completed✅"
 ]
 let { key } = await zk.sendMessage(dest, {text: 'Loading Please Wait'})
 
