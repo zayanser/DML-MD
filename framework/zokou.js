ed7beec5f3aabfd9f693ccece80d82239635ddbb
DML <daudymussa1705@gmail.com>
2025-05-10 08:43:43 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🏍️";
+        infoComs.reaction = "🚀";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
