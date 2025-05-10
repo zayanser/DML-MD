152f7dcc7a151b7b96effe3a19cfb314e3500d0d
DML <daudymussa1705@gmail.com>
2025-05-10 08:27:04 +0300

    Update Chbibles.js

--- commandes/Chbibles.js
+++ commandes/Chbibles.js
@@ -1,2 +1,145 @@
-const _0x19e3bb=_0x1e67;function _0x1e67(_0x4c9d0c,_0x55860a){const _0x5a7389=_0x5a73();return _0x1e67=function(_0x1e676a,_0x4b1a37){_0x1e676a=_0x1e676a-0x14d;let _0x5b0041=_0x5a7389[_0x1e676a];return _0x5b0041;},_0x1e67(_0x4c9d0c,_0x55860a);}function _0x5a73(){const _0x59faf6=['json','define','\x0a\x0a\x0a\x0a\x0a◇\x20_Engine\x20by:_\x20*CHARLES_KE*\x0a\x0a\x0a╔═════◇\x0a║◇\x20*KEEP\x20USING\x20CHARLES-MD*\x0a╚════════════════════>\x20','129708eQLvyo','https://favqs.com/api/qotd','1035832clLtsG','Please\x20specify\x20the\x20book,\x20the\x20chapter\x20and\x20the\x20verse\x20you\x20want\x20to\x20read.\x20Example:\x20bible\x20Romans\x206:23','\x0a\x0a\x0a╭────────────────◆\x0a│\x20*_Engine\x20by\x20CHARLES_KE._*\x0a╰─────────────────◆','General','https://bible-api.com/','https://nekos.life/api/v2/fact','3939299jcUWZv','◆━━━━━━✦FACT✦━━━━━━◆\x20\x0a*◇*\x20','fact','get','artist','quotes','User','quote','57564XIAlMQ','2727840bibcTM','Incorrect\x20format.\x0aExample:\x20poll\x20what\x20is\x201+1/2,\x203,\x204','body','songs','list','translation_name','*CHARLESKE-MD\x20LYRICS\x20FINDER*\x0a\x0a*TITLE*\x20-\x20','log','\x0a◆━━━━━━✦QUOTE✦━━━━━━◆\x20\x0a◇\x20_','35652aFoaFn','\x0a\x20Example:\x20','poll','split','author','push','definition','535aHIVHB','lyrics','\x0a\x0a\x0a\x0a\x0a*◇*\x20Engine\x20by\x20*Marisel*\x0a\x0a╔═════◇\x0a║◇\x20*KEEP\x20USING\x20CHARLESKE*\x0a╚════════════════════>\x20','9zXMYZN','\x0a\x0a🤍\x20*_NOW\x20READ:_*\x20','genius-lyrics','text','http://api.urbandictionary.com/v0/define?term=','search','join','No\x20result\x20for\x20','\x0a\x20Definition:\x20','jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO','📖\x20*THE\x20HOLY\x20BIBLE*\x0a\x0a📜\x20*_WE\x27RE\x20READING:_*\x20','Search','axios','Provide\x20a\x20term','sendMessage','../framework/zokou','Error\x20occurred:\x20','replace','Client','name','Please\x20specify\x20the\x20chapter\x20number\x20or\x20name.\x20Example:\x20bible\x20john\x203:16','_\x0a\x0a\x0a◇\x20*AUTHOR:*\x20','91749plDHMV','length','\x0a\x20Word:\x20'];_0x5a73=function(){return _0x59faf6;};return _0x5a73();}(function(_0x417d71,_0x38de13){const _0x4d730d=_0x1e67,_0x263565=_0x417d71();while(!![]){try{const _0x586bcb=-parseInt(_0x4d730d(0x172))/0x1+parseInt(_0x4d730d(0x188))/0x2+-parseInt(_0x4d730d(0x178))/0x3+-parseInt(_0x4d730d(0x189))/0x4+-parseInt(_0x4d730d(0x159))/0x5*(-parseInt(_0x4d730d(0x152))/0x6)+parseInt(_0x4d730d(0x180))/0x7+-parseInt(_0x4d730d(0x17a))/0x8*(-parseInt(_0x4d730d(0x15c))/0x9);if(_0x586bcb===_0x38de13)break;else _0x263565['push'](_0x263565['shift']());}catch(_0x1a01d7){_0x263565['push'](_0x263565['shift']());}}}(_0x5a73,0x83cdb));const {zokou}=require(_0x19e3bb(0x16b)),axios=require(_0x19e3bb(0x168)),Genius=require(_0x19e3bb(0x15e)),Client=new Genius[(_0x19e3bb(0x16e))](_0x19e3bb(0x165));zokou({'nomCom':'bible','reaction':'🎎','categorie':'General'},async(_0x7f80f7,_0x20d5c8,_0x31b9e8)=>{const _0xabcd81=_0x19e3bb,{repondre:_0x3b09b9,arg:arguments,ms:_0x2a01c7}=_0x31b9e8,_0x1fac81=arguments[_0xabcd81(0x162)]('\x20');if(!_0x1fac81)return _0x3b09b9(_0xabcd81(0x17b));let _0x37123e=await fetch(_0xabcd81(0x17e)+_0x1fac81);if(!_0x37123e['ok'])return _0x3b09b9(_0xabcd81(0x170));let _0x348044=await _0x37123e[_0xabcd81(0x175)](),_0x3d6778=_0xabcd81(0x166)+_0x348044['reference']+'\x0a\x0a🔢\x20*_NUMBER\x20OF\x20VERSES:_*\x20'+_0x348044['verses']['length']+_0xabcd81(0x15d)+_0x348044[_0xabcd81(0x15f)]+'\x0a\x0a🌍\x20*_LANGUAGE_:*\x20'+_0x348044[_0xabcd81(0x14e)]+_0xabcd81(0x17c);await _0x3b09b9(_0x3d6778);}),zokou({'nomCom':_0x19e3bb(0x154),'reaction':'✨','categorie':_0x19e3bb(0x17d)},async(_0x235de7,_0x464381,_0x128ec2)=>{const _0x617596=_0x19e3bb,{repondre:_0xca0209,arg:arguments,ms:_0x28e077}=_0x128ec2,_0x3e2a44=arguments['join']('\x20');let [_0x17961c,_0xeb656f]=_0x3e2a44[_0x617596(0x155)]('/');if(_0x3e2a44[_0x617596(0x155)]('/')[_0x617596(0x173)]<0x2)return _0xca0209(_0x617596(0x18a));let _0x4684e4=[];for(let _0x1a2cf8 of _0xeb656f['split'](',')){_0x4684e4[_0x617596(0x157)](_0x1a2cf8);}await _0x464381[_0x617596(0x16a)](_0x235de7,{'poll':{'name':_0x17961c,'values':_0x4684e4}});}),zokou({'nomCom':'fact','reaction':'✌️','categorie':_0x19e3bb(0x186)},async(_0x48f5b7,_0x1b081b,_0x4b7a64)=>{const _0x1c6d9a=_0x19e3bb,{repondre:_0x3259ab,arg:arguments,ms:_0x4e5e58}=_0x4b7a64,_0x56dc6e=await fetch(_0x1c6d9a(0x17f)),_0x5d4059=await _0x56dc6e[_0x1c6d9a(0x175)]();_0x3259ab(_0x1c6d9a(0x181)+_0x5d4059[_0x1c6d9a(0x182)]+_0x1c6d9a(0x15b));}),zokou({'nomCom':_0x19e3bb(0x185),'reaction':'🗿','categorie':'User'},async(_0x301bf3,_0x44389c,_0x43017d)=>{const _0x2ddeb5=_0x19e3bb,{repondre:_0x2b99ca,arg:arguments,ms:_0x24c28a}=_0x43017d,_0x3b7b59=await fetch(_0x2ddeb5(0x179)),_0x51c7ca=await _0x3b7b59[_0x2ddeb5(0x175)](),_0x245c98=_0x2ddeb5(0x151)+_0x51c7ca['quote'][_0x2ddeb5(0x18b)]+_0x2ddeb5(0x171)+_0x51c7ca[_0x2ddeb5(0x187)][_0x2ddeb5(0x156)]+_0x2ddeb5(0x177);_0x2b99ca(_0x245c98);}),zokou({'nomCom':_0x19e3bb(0x176),'reaction':'😁','categorie':_0x19e3bb(0x167)},async(_0x27def6,_0x42deee,_0x56a027)=>{const _0x1cf41d=_0x19e3bb,{repondre:_0x44ad38,arg:arguments,ms:_0x323d9e}=_0x56a027;if(!arguments||arguments['length']===0x0)return _0x44ad38(_0x1cf41d(0x169));const _0x5afa48=arguments[_0x1cf41d(0x162)]('\x20');try{let {data:_0x302865}=await axios[_0x1cf41d(0x183)](_0x1cf41d(0x160)+_0x5afa48);var _0x2263e1=_0x1cf41d(0x174)+_0x5afa48+_0x1cf41d(0x164)+_0x302865[_0x1cf41d(0x14d)][0x0][_0x1cf41d(0x158)][_0x1cf41d(0x16d)](/\[/g,'')[_0x1cf41d(0x16d)](/\]/g,'')+_0x1cf41d(0x153)+_0x302865[_0x1cf41d(0x14d)][0x0]['example'][_0x1cf41d(0x16d)](/\[/g,'')['replace'](/\]/g,'');return _0x44ad38(_0x2263e1);}catch{return _0x44ad38(_0x1cf41d(0x163)+_0x5afa48);}}),zokou({'nomCom':_0x19e3bb(0x15a),'reaction':'✨','categorie':_0x19e3bb(0x167)},async(_0x221c4b,_0x5d65bf,_0x477dcd)=>{const _0x478190=_0x19e3bb,{repondre:_0xdfbfa4,arg:arguments,ms:_0xe72815}=_0x477dcd;try{if(!arguments||arguments[_0x478190(0x173)]===0x0)return _0xdfbfa4('Please\x20provide\x20me\x20the\x20song\x20name');const _0x332e1b=arguments[_0x478190(0x162)]('\x20'),_0x54cab1=await Client[_0x478190(0x18c)][_0x478190(0x161)](_0x332e1b),_0x492476=_0x54cab1[0x0];console[_0x478190(0x150)](_0x492476);const _0x33b425=await _0x492476[_0x478190(0x15a)](),_0x5a235e=await _0x492476[_0x478190(0x184)][_0x478190(0x16f)],_0x4675e5=await _0x492476['title'],_0x510d7f=_0x478190(0x14f)+_0x4675e5+'\x0a\x0a*ARTIST*\x20-\x20'+_0x5a235e+'\x0a\x0a'+_0x33b425;await _0x5d65bf[_0x478190(0x16a)](_0x221c4b,{'image':{'url':'./media/lyrics.jpg'},'caption':_0x510d7f},{'quoted':_0xe72815});}catch(_0x1d245c){_0xdfbfa4(_0x478190(0x16c)+_0x1d245c),console['log'](_0x1d245c);}});
-https://favqs.com/api/qotd','1035832clLtsG','Pleasehttps://bible-api.com/','https://nekos.life/api/v2/fact','3939299jcUWZv','◆━━━━━━✦http://api.urbandictionary.com/v0/define?term=','search','join','No
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const Taphere = more.repeat(4001)
+
+zokou({ nomCom: "biblelist", categorie: "God-first" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework/zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault("Africa/Nairobi");
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+let infoMsg =  `
+
+🤲🕍  ┈─• *HOLY BIBLE* •─┈  🕍🤲
+
+ 💫 𝘈𝘭𝘭 Holy books 𝘢𝘯𝘥 𝘵𝘩𝘦𝘪𝘳 𝘯𝘶𝘮𝘣𝘦𝘳𝘴 𝘭𝘪𝘴𝘵
+𝘧𝘰𝘳 𝘨𝘦𝘵𝘵𝘪𝘯𝘨 books 𝘵𝘺𝘱𝘦 ${s.PREFIXE}bible judges 2:3 Or ${s.PREFIXE}biblie judges 3:6💫🌸 
+
+📜 *Old Testament.* 📜
+1. 🧬 Genesis (MWANZO)
+2. ♟️ Exodus (KUTOKA)
+3. 🕴️ Leviticus (WALAWI)
+4. 🔢 Numbers (HESABU)
+5. 🗞️ Deuteronomy (TORATI)
+6. 🍁 Joshua (JOSHUA)
+7. 👨‍⚖️ Judges (WAAMUZI)
+8. 🌹 Ruth (RUTH)
+9. 🥀 1 Samuel (1SAMWELI)
+10. 🌺 2 Samuel (2 SAMWEL)
+11. 🌷 1 Kings (1 WAFALME)
+12. 👑 2 Kings(2 WAFALME)
+13. 🪷 1 Chronicles (1 WATHESALONIKE)
+14. 🌸 2 Chronicles (2 WATHESALONIKE)
+15. 💮 Ezra (EZRA)
+16. 🏵️ Nehemiah (NEHEMIA)
+17. 🌻 Esther (ESTA)
+18. 🌼 Job (AYUBU)
+19. 🍂 Psalms (ZABURI)
+20. 🍄 Proverbs (MITHALI)
+21. 🌾 Ecclesiastes (MHUBIRI)
+22. 🌱 Song of Solomon (WIMBO WA SULEMAN)
+23. 🌿 Isaiah (ISAYA)
+24. 🍃 Jeremiah (YEREMIA)
+25. ☘️ Lamentations (MAOMBOLEZO)
+26. 🍀 Ezekiel (EZEKIEL)
+27. 🪴 Daniel (DANIEL)
+28. 🌵 Hosea (HESEA)
+29. 🌴 Joel (JOEL)
+30. 🌳 Amos (AMOSI)
+31. 🌲 Obadiah (OBADIA)
+32. 🪵 Jonah (YONA)
+33. 🪹 Micah (MIKA)
+34. 🪺 Nahum (NAHUM)
+35. 🏜️ Habakkuk (HABAKUKI)
+36. 🏞️ Zephaniah (ZEFANIA)
+37. 🏝️ Haggai (HAGAI)
+38. 🌅 Zechariah (ZAKARIA)
+39. 🌄 Malachi (MALAKI)
+
+📖 *New Testament.* 📖
+1. 🌈 Matthew (MATHAYO)
+2. ☔ Mark (MARKO)
+3. 💧 Luke (LUKA)
+4. ☁️ John (JOHN)
+5. 🌨️ Acts (MATENDO)
+6. 🌧️ Romans (WARUMI)
+7. 🌩️ 1 Corinthians (1 WAKORITHO)
+8. 🌦️ 2 Corinthians (2 WAKORITHO)
+9. ⛈️ Galatians (WAGALATIA)
+10. 🌥️ Ephesians (WAEFESO)
+11. ⛅ Philippians (WAFILIPI)
+12. 🌤️ Colossians (WAKOLOSAI)
+13. ☀️ 1 Thessalonians (1 WATHESALONIKE)
+14. 🪐 2 Thessalonians (2WATHESALONIKE)
+15. 🌞 1 Timothy (TIMOTHEO)
+16. 🌝 2 Timothy (2TIMOTHEO)
+17. 🌚 Titus (TITO)
+18. 🌜 Philemon (FILEMONI)
+19. 🌛 Hebrews (WAEBRANIA)
+20. ⭐ James (JAMES)
+21. 🌟 1 Peter (1 PETER)
+22. ✨ 2 Peter (2 PETER)
+23. 💫 1 John (1 JOHN)
+24. 🌙 2 John (2JOHN)
+25. ☄️ 3 John (3 JOHN)
+26. 🌠 Jude (YUDA)
+27. 🌌 Revelation (UFUNUO WA YOHANA)
+
+
+🎮BY *DML TECH*📔
+`;
+    
+let menuMsg = `
+‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎
+> *POWERED BY DML-MD*
+
+> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ dml 
+
+ `;
+
+       try {
+        const senderName = nomAuteurMessage || message.from;  // Use correct variable for sender name
+        await zk.sendMessage(dest, {
+            text: infoMsg + menuMsg,
+            contextInfo: {
+                mentionedJid: [senderName],
+                externalAdReply: {
+                    title: "*DML-MD* HOLY BIBLE LIST",
+                    body: "Live with God my friends you don't know your tomorrow",
+                    thumbnailUrl: "https://files.catbox.moe/vcdwmp.jpg",
+                    sourceUrl: "https://whatsapp.com/channel/0029Vb2hoPpDZ4Lb3mSkVI3C",
+                    mediaType: 1,
+                    renderLargerThumbnail: true
+                }
+            }
+        });
+    } catch (error) {
+        console.error("Menu error: ", error);
+        repondre("🥵🥵 Menu error: " + error);
+    }
+});
