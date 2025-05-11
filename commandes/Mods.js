8ee4b437663d06ae6873af59d66a72f448b68a16
DML <daudymussa1705@gmail.com>
2025-05-12 00:23:25 +0300

    Update Mods.js

--- commandes/Mods.js
+++ commandes/Mods.js
@@ -622,7 +622,7 @@ zokou({
 
   if (alldata.length === 0 ) { repondre(`To activate or modify the mention; follow this syntax: mention link type message
   The different types are audio, video, image, and sticker.
-  Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is Beltah`) ; return}
+  Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is Dml`) ; return}
 
       if(data.status == 'non') {
           etat = 'Desactived'
