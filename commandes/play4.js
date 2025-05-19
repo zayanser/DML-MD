3c2d8d973b8270868b1071ef58a0275f85bc0c4c
DML <daudymussa1705@gmail.com>
2025-05-19 23:02:26 +0300

    Update and rename play4[1].js to play4.js

--- commandes/play4.js
+++ commandes/play4.js
@@ -48,7 +48,7 @@ _Reply to this message with 1 or 2 to download._`;
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363288304618280@newsletter',
-                newsletterName: 'NEXUS TECH',
+                newsletterName: 'DML TECH',
                 serverMessageId: 143
             }
         };
@@ -149,7 +149,7 @@ _Reply with 1, 2 or 3 to this message to download the format you prefer._`;
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363288304618280@newsletter',
-                newsletterName: 'NEXUS XMD',
+                newsletterName: 'DML MD',
                 serverMessageId: 143
             }
         };
